2cdcc7c7c14ce98400f2301a62785323398d5ec6
Eric Putnam <putnam.eric@gmail.com>
2019-10-01 14:39:59 -0700

    CDPE-2203 adds wait_for_approval method
    This method waits for deployment approval by blocking plan execution where ever it is called. It also takes a block with  which is the URL for the deployment details page

---
+++ lib/puppet/functions/cd4pe_deployments/wait_for_approval.rb
@@ -0,0 +1,75 @@
+require 'puppet_x/puppetlabs/cd4pe_client'
+require 'puppet_x/puppetlabs/cd4pe_function_result'
+require 'bolt/error'
+
+# @summary Blocks further plan execution until the deployment is approved in CD4PE
+#
+# Blocks further plan execution until the deployment is approved in CD4PE and takes a lambda that is executed once
+# at the start of the wait time. The lambda includes the "url" which is a link to the approval page for the deployment. If
+# the max approval window is exceeded (24 hours) or approval is declined, a Bolt::PlanFailure is raised, otherwise a result
+# is returned to the user.
+Puppet::Functions.create_function(:'cd4pe_deployments::wait_for_approval') do
+  # @param block
+  #   Takes a block that provides the URL to the deployment's approval page
+  # @example Notify Slack users that approval is needed
+  #   wait_for_approval do |String $url|
+  #     run_task("slack::notify", "#it-ops", "Please review this deployment for approval: ${url}")
+  #   end
+  # @return [CD4PEFunctionResult] result
+  #
+  dispatch :wait_for_approval do
+    block_param
+  end
+
+  def wait_for_approval(&block)
+    init_client
+    current_time = Time.now
+    max_time = current_time + 24 * 60 * 60
+
+    approval_response = approval_pending?
+    return approval_response unless approval_response[:error].nil?
+
+    if approval_response[:result].empty? # rubocop:disable Style/GuardClause
+      url = approval_url
+      block.call(url) if block_given? && !url.nil? # rubocop:disable Performance/RedundantBlockCall
+
+      while approval_response[:result].empty? && current_time < max_time
+        current_time = Time.now
+        approval_response = approval_pending?
+        sleep(5)
+      end
+
+      raise Bolt::PlanFailure.new("Approval timed out for deployment #{ENV['DEPLOYMENT_ID']}", 'bolt/plan-failure') if approval_response[:result].empty?
+      raise Bolt::PlanFailure.new("Deployment #{ENV['DEPLOYMENT_ID']} declined", 'bolt/plan-failure') if approval_decision(approval_response) == 'DECLINED'
+      approval_response
+    end
+  end
+
+  def approval_pending?
+    response = @client.get_approval_state
+
+    if response.code == '200'
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      return PuppetX::Puppetlabs::CD4PEFunctionResult.create_result(response_body)
+    elsif response.code =~ %r{4[0-9]+}
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      return PuppetX::Puppetlabs::CD4PEFunctionResult.create_error_result(response_body)
+    else
+      raise Puppet::Error "Unknown HTTP Error with code: #{response.code} and body #{response.body}"
+    end
+  rescue => exception
+    PuppetX::Puppetlabs::CD4PEFunctionResult.create_exception_result(exception)
+  end
+
+  def approval_url
+    "#{@client.config[:scheme]}://#{@client.config[:server]}:#{@client.config[:port]}/#{@client.config[:deployment_owner]}/deployments/#{@client.config[:deployment_id]}"
+  end
+
+  def approval_decision(response)
+    response[:result][:approvalDecision]
+  end
+
+  def init_client
+    @client = PuppetX::Puppetlabs::CD4PEClient.new
+  end
+end
---
+++ spec/functions/cd4pe/wait_for_approval_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+require_relative '../../../lib/puppet/functions/cd4pe_deployments/wait_for_approval'
+require 'webmock/rspec'
+
+describe 'cd4pe_deployments::wait_for_approval' do
+  context 'table steaks' do
+    include_context 'deployment'
+
+    it 'exists' do
+      is_expected.not_to eq(nil)
+    end
+
+    it 'runs with a lambda' do
+      stub_request(:get, ajax_url)
+        .with(query: { op: 'GetDeploymentApprovalState', deploymentId: deployment_id }, headers: { 'authorization' => "Bearer token #{ENV['DEPLOYMENT_TOKEN']}" })
+        .to_return(body: JSON.generate(response))
+        .times(1)
+
+      is_expected.to(run.with_lambda { |url| puts url })
+    end
+  end
+
+  context 'happy' do
+    include_context 'deployment'
+
+    let(:response) do
+      {
+        approvalDecision: 'APPROVED',
+      }
+    end
+
+    let(:response404) do
+      {
+        error: {
+          message: 'Something went wrong, make sure your seatbelt is securely fastened',
+          code: 'FunctionFailure',
+        },
+      }
+    end
+
+    it 'succeeds with parameters' do
+      stub_request(:get, ajax_url)
+        .with(query: { op: 'GetDeploymentApprovalState', deploymentId: deployment_id }, headers: { 'authorization' => "Bearer token #{ENV['DEPLOYMENT_TOKEN']}" })
+        .to_return(body: JSON.generate(response))
+        .times(1)
+
+      is_expected.to(run.with_lambda { |url| puts url })
+    end
+
+    it 'returns error response with 404 code' do
+      stub_request(:get, ajax_url)
+        .with(query: { op: 'GetDeploymentApprovalState', deploymentId: deployment_id }, headers: { 'authorization' => "Bearer token #{ENV['DEPLOYMENT_TOKEN']}" })
+        .to_return(body: JSON.generate(response404), status: 404)
+        .times(1)
+
+      is_expected.to run.with_lambda { |url| puts url }.and_return(
+        result: nil,
+        error: {
+          message: 'Something went wrong, make sure your seatbelt is securely fastened',
+          code: 'FunctionFailure',
+        },
+      )
+    end
+
+    it 'returns error response with 500 code' do
+      stub_request(:get, ajax_url)
+        .with(query: { op: 'GetDeploymentApprovalState', deploymentId: deployment_id }, headers: { 'authorization' => "Bearer token #{ENV['DEPLOYMENT_TOKEN']}" })
+        .to_return(body: 'nobody', status: 500)
+        .times(1)
+
+      is_expected.to run.with_lambda { |url| puts url }.and_return(
+        result: nil,
+        error: {
+          message: 'Encountered exception: Received 3 server error responses from the CD4PE service at http://puppet.test:80: 500 nobody',
+          code: 'EncounteredException',
+        },
+      )
+    end
+
+    it 'behaves appropriately when receiving an empty response' do
+      stub_request(:get, ajax_url)
+        .with(query: { op: 'GetDeploymentApprovalState', deploymentId: deployment_id }, headers: { 'authorization' => "Bearer token #{ENV['DEPLOYMENT_TOKEN']}" })
+        .to_return({ body: JSON.generate({}), status: 200 }, body: JSON.generate(response), status: 200)
+        .times(2)
+
+      is_expected.to run.with_lambda { |url| puts url }.and_return(
+        result: { approvalDecision: 'APPROVED' },
+        error: nil,
+      )
+    end
+  end
+end
