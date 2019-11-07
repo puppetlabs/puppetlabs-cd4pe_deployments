bd313d5d9026e038aee86097f18aac777823bd93
Eric Zounes <eric.zounes@puppet.com>
2019-11-07 14:20:32 -0800

    (CDPE-2488)(CDPE-2489) Add eventual consistency and feature branch plans
    - Adds initial eventual consistency and feature branch plans.
    - The logic only works for control repo Deployments.
    - Hasn't been tested.

--- plans/eventual_consistency.pp
+++ plans/eventual_consistency.pp
@@ -1,4 +1,8 @@
 plan cd4pe_deployments::eventual_consistency (
 ) {
-  #stuff
+  $target_node_group = get_node_group(system::env('NODE_GROUP_ID'))
+  $deploy_code_result = deploy_code($target_node_group['environment'])
+  if $deploy_code_result['error'] != undef {
+    fail_plan($deploy_code_result['error']['message'], $deploy_code_result['error']['code'])
+  }
 }
