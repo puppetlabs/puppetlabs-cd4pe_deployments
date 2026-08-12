2dfe86e2d3b8e93ad5ae7509f23a3a8e1e80b4c8
owenbeckles <becklesoweno@gmail.com>
2026-08-12 12:59:14 -0400

    (CDPE-7375) Update PDK template to 3.8.0
    Refreshes tooling (Gemfile, Rakefile, rubocop/lint configs, spec
    helpers) to the current PDK template ahead of Puppet 9 support work.
    Bumps the GitHub Actions runner to Ruby 3.1 to match, since the
    updated toolchain no longer installs under Ruby 2.6. Preserves the
    module's custom webmock dependency and bolt run_plan test wiring via
    .sync.yml and spec_helper_local.rb, which pdk update doesn't manage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 
@@ -19,27 +25,41 @@ default_fact_files.each do |f|
   next unless File.exist?(f) && File.readable?(f) && File.size?(f)
 
   begin
-    default_facts.merge!(YAML.safe_load(File.read(f), [], [], true))
-  rescue => e
+    require 'deep_merge'
+    default_facts.deep_merge!(YAML.safe_load_file(f, permitted_classes: [], permitted_symbols: [], aliases: true))
+  rescue StandardError => e
     RSpec.configuration.reporter.message "WARNING: Unable to load #{f}: #{e}"
   end
 end
 
 # read default_facts and merge them over what is provided by facterdb
 default_facts.each do |fact, value|
-  add_custom_fact fact, value
+  add_custom_fact fact, value, merge_facts: true
 end
 
 RSpec.configure do |c|
   c.default_facts = default_facts
-  c.mock_with :rspec
   c.before :each do
     # set to strictest setting for testing
     # by default Puppet runs at warning level
     Puppet.settings[:strict] = :warning
+    Puppet.settings[:strict_variables] = true
   end
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
+    RSpec::Puppet::Coverage.report!(0)
+  end
+
+  # Filter backtrace noise
+  backtrace_exclusion_patterns = [
+    %r{spec_helper},
+    %r{gems},
+  ]
+
+  if c.respond_to?(:backtrace_exclusion_patterns)
+    c.backtrace_exclusion_patterns = backtrace_exclusion_patterns
+  elsif c.respond_to?(:backtrace_clean_patterns)
+    c.backtrace_clean_patterns = backtrace_exclusion_patterns
   end
 end
 
@@ -49,53 +69,6 @@ def ensure_module_defined(module_name)
   module_name.split('::').reduce(Object) do |last_module, next_module|
     last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module, false)
     last_module.const_get(next_module, false)
-  end
-end
-
-RSpec.shared_context 'deployment' do
-  let(:test_host) { 'http://puppet.test' }
-  let(:deployment_owner) { 'ccaum' }
-  let(:deployment_id) { '123' }
-  let(:deployment_domain) { 'd25' }
-  let(:deployment_token) { '1234abcd' }
-  let(:node_group_id) { 'aasdf-1234asdf-1234' }
-  let(:environment_name) { 'development' }
-  let(:control_repo) { 'test_control_repo' }
-  let(:commit) { 'ef424ec352d4bc93317be901877e32f3c6a0289c' }
-  let(:git_branch) { 'src_development' }
-  let(:ajax_url) { "#{test_host}/#{deployment_owner}/ajax" }
-  let(:api_v1_path) { "#{test_host}/api/v1" }
-  let(:response) do
-    {
-      'result' => {
-        'name' => 'deployment',
-        'id' => '123',
-        'description' => 'carls cool deployment',
-      },
-      'error' => nil,
-    }
-  end
-
-  let(:error_response) do
-    {
-      'result' => nil,
-      'error' => {
-        'message' => 'Some error message',
-        'code' => 'ErrorCode',
-      },
-    }
-  end
-
-  before(:each) do
-    ENV['DEPLOYMENT_OWNER'] = deployment_owner
-    ENV['DEPLOYMENT_ID'] = deployment_id
-    ENV['DEPLOYMENT_DOMAIN'] = deployment_domain
-    ENV['DEPLOYMENT_TOKEN'] = deployment_token
-    ENV['WEB_UI_ENDPOINT'] = test_host
-    ENV['REPO_TARGET_BRANCH'] = environment_name
-    ENV['COMMIT'] = commit
-    ENV['CONTROL_REPO'] = control_repo
-    ENV['NODE_GROUP_ID'] = node_group_id
   end
 end
 
--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -12,3 +12,50 @@ if Gem::Version.new(Puppet.version) >= Gem::Version.new('6.0.0')
     warn '=== bolt tests will not run; ensure bolt gem is installed (requires Puppet 6+)'
   end
 end
+
+RSpec.shared_context 'deployment' do
+  let(:test_host) { 'http://puppet.test' }
+  let(:deployment_owner) { 'ccaum' }
+  let(:deployment_id) { '123' }
+  let(:deployment_domain) { 'd25' }
+  let(:deployment_token) { '1234abcd' }
+  let(:node_group_id) { 'aasdf-1234asdf-1234' }
+  let(:environment_name) { 'development' }
+  let(:control_repo) { 'test_control_repo' }
+  let(:commit) { 'ef424ec352d4bc93317be901877e32f3c6a0289c' }
+  let(:git_branch) { 'src_development' }
+  let(:ajax_url) { "#{test_host}/#{deployment_owner}/ajax" }
+  let(:api_v1_path) { "#{test_host}/api/v1" }
+  let(:response) do
+    {
+      'result' => {
+        'name' => 'deployment',
+        'id' => '123',
+        'description' => 'carls cool deployment',
+      },
+      'error' => nil,
+    }
+  end
+
+  let(:error_response) do
+    {
+      'result' => nil,
+      'error' => {
+        'message' => 'Some error message',
+        'code' => 'ErrorCode',
+      },
+    }
+  end
+
+  before(:each) do
+    ENV['DEPLOYMENT_OWNER'] = deployment_owner
+    ENV['DEPLOYMENT_ID'] = deployment_id
+    ENV['DEPLOYMENT_DOMAIN'] = deployment_domain
+    ENV['DEPLOYMENT_TOKEN'] = deployment_token
+    ENV['WEB_UI_ENDPOINT'] = test_host
+    ENV['REPO_TARGET_BRANCH'] = environment_name
+    ENV['COMMIT'] = commit
+    ENV['CONTROL_REPO'] = control_repo
+    ENV['NODE_GROUP_ID'] = node_group_id
+  end
+end
